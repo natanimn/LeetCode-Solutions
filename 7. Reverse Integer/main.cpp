d12ead0e7b6e92fb83177a85f93b52ad91da98be
natanimn <natanimco@gmail.com>
2026-02-12 14:58:30 +0300

    Solution

---
+++ 7. Reverse Integer/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class Solution {
+public:
+    int reverse(int x) {
+        long long res = 0;
+        while (x!=0) {
+            res += (x % 10) ;
+            res*=10;
+            x /= 10;
+        }
+        res /= 10;
+        if (res > INT_MAX || res < INT_MIN)
+            return 0;
+        return res;
+    }
+};
+
+int main() {
+    Solution solution;
+    std::cout << solution.reverse(120);
+    return 0;
+}
